186e85913782ccd9239dcfe20609b0b88d0cc728
Harangozó Csaba <csaba.harangozo@dbx.hu>
2010-09-03 19:21:34 +0200

    durationType preprocessben, határozottra is működik

--- src/main/java/hu/dbx/screek/model/State.java
+++ src/main/java/hu/dbx/screek/model/State.java
@@ -3,25 +3,29 @@ package hu.dbx.screek.model;
 public class State {
 	
 	public enum States {
-			WF_START, 
+			WF_START,
+			WF_VALIDATE,
 			WF_CHECK_VALID,
+			WF_CALCULATE,			
 			WF_CLEANUP,
 			WF_END,
-			VALID_TARIFF_START,
-			VALID_TARIFF_END,
-			CALCULATE_START,
-			CALCULATE_END,
-			CLEANUP_START,
-			CLEANUP_END
+			AGENDA_PREPROCESS_START,
+			AGENDA_PREPROCESS_END,
+			AGENDA_VALIDATE_START,
+			AGENDA_VALIDATE_END,
+			AGENDA_CALCULATE_START,
+			AGENDA_CALCULATE_END,
+			AGENDA_CLEANUP_START,
+			AGENDA_CLEANUP_END
 	}
 
-
+/*
 	public static final int WF_START = 0;
 	public static final int WF_CHECK_VALID = 1;
 	public static final int VALID_TARIFF_START = 2;
 	public static final int VALID_TARIFF_END = 3;
 	public static final int TARIFFENDED = 4;
-	
+*/	
 	private States state;
 	
 	public State(States state) {
