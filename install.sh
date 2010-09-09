fa389994cbf6ad394a99affdd5b6f9013c2359e3
Harangozó Csaba <csaba.harangozo@dbx.hu>
2010-09-09 16:07:05 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-scp -r -i /home/kishari/.ssh/vvdev.pem /home/kishari/screek/rules ubuntu@aws-3.dbx.hu:/home/ubuntu/screek
-scp -i /home/kishari/.ssh/vvdev.pem /home/kishari/screek/log4j.properties ubuntu@aws-3.dbx.hu:/home/ubuntu/glassfishv3/glassfish/domains/screek/lib/classes
-scp -i /home/kishari/.ssh/vvdev.pem /home/kishari/screek/screek.properties ubuntu@aws-3.dbx.hu:/home/ubuntu/glassfishv3/glassfish/domains/screek/lib/classes
-scp -i /home/kishari/.ssh/vvdev.pem /home/kishari/screek/screek.war ubuntu@aws-3.dbx.hu:/home/ubuntu/glassfishv3/glassfish/domains/screek/autodeploy
+domainDir="$HOME/glassfishv3/glassfish/domains/screek"
+#echo $domainDir
+domainClassesDir="$domainDir/lib/classes"
+#echo $domainClassesDir
+
+knowledgeBasePath="$HOME/screek/StrawCreek/src/main/rules"
+echo "knowledgeBasePath=$knowledgeBasePath" > screek.properties
+
+ant -f build.xml deploy
+
+Move screek.properties file to $domainClassesDir directory...
+mv screek.properties $domainClassesDir
