67f8ac707a7d9fc00f8ef2e7847e0bd0decc62ae
Harangozó Csaba <csaba.harangozo@dbx.hu>
2010-10-21 13:12:10 +0200

    save (díjfizetés módja 0 - átutalás helyett 3 - átutalás) díjfizetés módja még integer

--- src/main/java/hu/dbx/screek/model/Constants.java
+++ src/main/java/hu/dbx/screek/model/Constants.java
@@ -6,22 +6,22 @@ public class Constants {
 	public static final Integer DT_HATAROZOTT = 2;
 	
 	//Jármű típusok (Vehicle.typeCode)
-	public static final String TC_SZEMELYGEPJARMU = "1";
-	public static final String TC_TEHERGEPJARMU = "2";
-	public static final String TC_MOTORKEREKPAR = "3";
-	public static final String TC_AUTOBUSZ = "4";
-	public static final String TC_TROLIBUSZ = "5";
-	public static final String TC_VONTATO = "6";
-	public static final String TC_KONNYU_POTKOCSI = "7";
-	public static final String TC_NEHEZ_POTKOCSI = "8";
-	public static final String TC_SZEMELYGEPKOCSI_UTANFUTO = "10";
-	public static final String TC_LAKOKOCSI = "11";
-	public static final String TC_MOTORKEREKPAR_UTANFUTO = "12";
-	public static final String TC_MEZOGAZDASAGI_VONTATO = "13";
-	public static final String TC_LASSUJARMU = "14";
-	public static final String TC_MUNKAGEP = "15";
-	public static final String TC_SEGEDMOTOROS_KEREKPAR = "16";
-	public static final String TC_PROBA_RENDSZAM = "17";
+	public static final String TC_SZEMELYGEPJARMU = "10";
+	public static final String TC_TEHERGEPJARMU = "14";
+	public static final String TC_MOTORKEREKPAR = "11";
+	public static final String TC_AUTOBUSZ = "12";
+	public static final String TC_TROLIBUSZ = "13";
+	public static final String TC_VONTATO = "15";
+	public static final String TC_KONNYU_POTKOCSI = "16";
+	public static final String TC_NEHEZ_POTKOCSI = "17";
+	public static final String TC_SZEMELYGEPKOCSI_UTANFUTO = "18";
+	public static final String TC_LAKOKOCSI = "19";
+	public static final String TC_MOTORKEREKPAR_UTANFUTO = "20";
+	public static final String TC_MEZOGAZDASAGI_VONTATO = "21";
+	public static final String TC_LASSUJARMU = "22";
+	public static final String TC_MUNKAGEP = "23";
+	public static final String TC_SEGEDMOTOROS_KEREKPAR = "24";
+	public static final String TC_PROBA_RENDSZAM = "27";
 	
 
 	
@@ -37,8 +37,9 @@ public class Constants {
 	public static final Integer PF_EVES = 1;
 	
 	//Fizetési mód (Quote.paymentMethod)
+	public static final Integer PM_ATUTALAS = 3;
 	public static final Integer PM_BANKI_CSOPORTOS_BESZEDES = 1;
-	public static final Integer PM_EGYEB = 2;
+	public static final Integer PM_POSTAI_CSEKK = 2;
 	
 	//Üzemeltetési jelleg (Vehicle.operationalModality)
 	public static final Integer OM_NORMAL = 1;
